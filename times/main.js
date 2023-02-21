51b19b64e303bea981ca31aa0a0b49bde2e0b074
whynotBb <whynot.byoul@gmail.com>
2023-02-21 18:02:44 +0900

    pagination 기능 추가

--- times/main.js
+++ times/main.js
@@ -1,57 +1,60 @@
+let news = [];
+let page = 1;
+let total_pages = 0;
 const menus = document.querySelectorAll(".menus button");
-const searchTxt = document.getElementById("searchTxt");
 const searchBtn = document.getElementById("searchBtn");
-let searchItem = "";
-
-searchBtn.addEventListener("click", () => {
-  searchItem = searchTxt.value;
-  console.log(searchItem);
-  getLatestNews(searchItem);
-});
-
-console.log(menus);
+let url;
 let category = "";
+
 menus.forEach((item, idx) => {
   item.addEventListener("click", function () {
     category = this.innerText.toLowerCase();
     getLatestNews(category);
   });
 });
-console.log(category);
 
-let news = [];
+const getNews = async () => {
+  try {
+    let header = new Headers({
+      // "x-api-key": "hjmgah6VkSooZdDWfDsr-t9xAv42iwr-52iPPAHRBcE",
+      "x-api-key": "LmCKjCVnD5EtPCcf-my1JEmJ3dP-w1t1spjxT-nAQto",
+    });
+    url.searchParams.set("page", page); //&page=page변수의값
+    console.log("url=", url);
+    let response = await fetch(url, { headers: header });
+    let data = await response.json();
+    if (response.status == 200) {
+      console.log("data = ", data);
+      if (data.total_hits == 0) {
+        throw new Error("검색된 결과 값이 없습니다.");
+      }
+      news = data.articles;
+      total_pages = data.total_pages;
+      page = data.page;
+      render();
+      pagination();
+    } else {
+      throw new Error(data.message);
+    }
+  } catch (error) {
+    console.log("catch error", error.message);
+    errorRender(error.message);
+  }
+};
 
 const getLatestNews = async (category) => {
-  let url = new URL(
+  url = new URL(
     `https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=${category}&page_size=10`
   );
-
-  let header = new Headers({
-    "x-api-key": "hjmgah6VkSooZdDWfDsr-t9xAv42iwr-52iPPAHRBcE",
-  });
-  let response = await fetch(url, { headers: header }); // ajax, http , fetch ( 데이터를 받는 방법)
-  let data = await response.json();
-  // async => await 셋트 : 시간이 걸리는 일을 할 때 기다려줘!/ 다음으로 넘기지 말고, 가져올 때 까지 기다리기
-  news = data.articles;
-
-  render();
-  console.log(news);
+  getNews();
 };
-const getSearchNews = async (searchItem) => {
-  let url = new URL(
-    `https://api.newscatcherapi.com/v2/search?q=${searchItem}&from=2021/12/15&countries=CA&page_size=1`
+
+const getNewsByKeyword = async () => {
+  let keyword = document.getElementById("searchTxt").value;
+  url = new URL(
+    `https://api.newscatcherapi.com/v2/search?q=${keyword}&page_size=10`
   );
-
-  let header = new Headers({
-    "x-api-key": "hjmgah6VkSooZdDWfDsr-t9xAv42iwr-52iPPAHRBcE",
-  });
-  let response = await fetch(url, { headers: header }); // ajax, http , fetch ( 데이터를 받는 방법)
-  let data = await response.json();
-  // async => await 셋트 : 시간이 걸리는 일을 할 때 기다려줘!/ 다음으로 넘기지 말고, 가져올 때 까지 기다리기
-  news = data.articles;
-
-  render();
-  console.log(news);
+  getNews();
 };
 
 const render = () => {
@@ -88,4 +91,55 @@ const render = () => {
 
   document.getElementById("news-board").innerHTML = newsHTML;
 };
+
+const errorRender = (message) => {
+  let errorHtml = `<div class="alert alert-danger text-center mt-4" role="alert">
+  ${message}
+</div>`;
+  document.getElementById("news-board").innerHTML = errorHtml;
+};
+
+const pagination = () => {
+  let paginationHtml = "";
+  let pageGroup = Math.ceil(page / 5);
+  const last = pageGroup * 5;
+  const first = last - 4;
+  paginationHtml = `
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Previous" moveToPrev()>
+        <span aria-hidden="true">&laquo;</span>
+      </a>
+    </li>
+  `;
+  for (let i = first; i <= last; i++) {
+    paginationHtml += `
+      <li class="page-item"><a class="page-link ${
+        page == i ? "active" : ""
+      }" href="#" onclick="moveToPage(${i})">${i}</a></li>
+    `;
+  }
+
+  document.querySelector(".pagination").innerHTML = paginationHtml;
+};
+
 getLatestNews(category);
+const moveToPage = (pageNum) => {
+  page = pageNum;
+
+  getNews();
+};
+const moveToPrev = () => {
+  if (page > 1) {
+    page = page - 1;
+    getNews();
+  }
+};
+const moveToNext = () => {
+  // if (page < ) {
+
+  // }
+  page = page + 1;
+
+  getNews();
+};
+searchBtn.addEventListener("click", getNewsByKeyword);
