8ff08dc60de203307cbe4f8e2f5ff0f6370f59f2
bb <whynot.byoul@gmail.com>
2023-02-23 07:49:55 +0900

    현재위치가져오기 기능

--- weather_project/README.md
+++ weather_project/README.md
@@ -6,3 +6,11 @@ https://www.pexels.com/ko-kr/search/videos/Clear%20sky/
 
 icon
 https://fonts.google.com/
+
+todo
+
+1. 현재 시간 실시간으로 보여주기(2/22)
+2. 현재 위치 가져와서 기본 날씨 보여주기
+3. 돋보기 아이콘 클릭 시 input box 노출 되며 도시 검색 기능 제공
+4. 참고
+   https://velog.io/@chloe_park/Javascript-14.weather-API-%ED%98%B8%EC%B6%9C%ED%95%98%EA%B8%B0
--- weather_project/js/main.js
+++ weather_project/js/main.js
@@ -1,4 +1,5 @@
-let realTime = document.querySelector(".now");
+//1. 현재 시간 실시간 보여주기
+const realTime = document.querySelector(".now");
 
 const getClock = () => {
   const now = new Date();
@@ -18,3 +19,32 @@ ${year}-${month < 10 ? `0${month}` : month}-${date < 10 ? `0${date} ` : date} ${
 };
 getClock();
 setInterval(getClock, 1000);
+//2. 현재 위치 가져와서 기본 날씨 보여주기
+let url;
+const options = {
+  enableHighAccuracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
+
+function success(pos) {
+  const crd = pos.coords;
+
+  console.log("Your current position is:");
+  console.log(`Latitude : ${crd.latitude}`);
+  console.log(`Longitude: ${crd.longitude}`);
+  console.log(`More or less ${crd.accuracy} meters.`);
+}
+
+function error(err) {
+  console.warn(`ERROR(${err.code}): ${err.message}`);
+}
+
+navigator.geolocation.getCurrentPosition(success, error, options);
+
+const getWeather = () => {
+  url = new URL(
+    `https://api.openweathermap.org/data/3.0/onecall?lat=${crd.latitude}&lon=${crd.longitude}&exclude=hourly,daily&appid=6dd95cfc5f180ab7bf62671b417e6c68`
+  );
+};
+console.log(url);
