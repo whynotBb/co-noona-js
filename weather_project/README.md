68c7f2a96ce55ccbba158438ade9462f2ec56be4
whynotBb <whynot.byoul@gmail.com>
2023-02-23 18:02:14 +0900

    검색기능 추가(진행중)

--- weather_project/README.md
+++ weather_project/README.md
@@ -14,3 +14,26 @@ todo
 3. 돋보기 아이콘 클릭 시 input box 노출 되며 도시 검색 기능 제공
 4. 참고
    https://velog.io/@chloe_park/Javascript-14.weather-API-%ED%98%B8%EC%B6%9C%ED%95%98%EA%B8%B0
+
+섭씨 = (Kelvin u2013 273.15)
+켈빈 = (섭씨 + 273.15)
+
+main 날씨 icon으로 나타내야 하는 case list 확인
+clear
+clouds
+rain
+thunderstorm
+snow
+mist
+Drizzle
+
+mist
+Smoke
+Haze
+Dust
+Fog
+Sand
+Dust
+Ash
+Squall
+Tornado
--- weather_project/js/main.js
+++ weather_project/js/main.js
@@ -21,6 +21,11 @@ getClock();
 setInterval(getClock, 1000);
 //2. 현재 위치 가져와서 기본 날씨 보여주기
 let url;
+let lat;
+let lon;
+let weather;
+let city;
+let weather_temps;
 const options = {
   enableHighAccuracy: true,
   timeout: 5000,
@@ -29,11 +34,9 @@ const options = {
 
 function success(pos) {
   const crd = pos.coords;
-
-  console.log("Your current position is:");
-  console.log(`Latitude : ${crd.latitude}`);
-  console.log(`Longitude: ${crd.longitude}`);
-  console.log(`More or less ${crd.accuracy} meters.`);
+  lat = crd.latitude;
+  lon = crd.longitude;
+  getWeather();
 }
 
 function error(err) {
@@ -42,9 +45,74 @@ function error(err) {
 
 navigator.geolocation.getCurrentPosition(success, error, options);
 
-const getWeather = () => {
+const getWeather = async () => {
+  const API_key = "6dd95cfc5f180ab7bf62671b417e6c68";
   url = new URL(
-    `https://api.openweathermap.org/data/3.0/onecall?lat=${crd.latitude}&lon=${crd.longitude}&exclude=hourly,daily&appid=6dd95cfc5f180ab7bf62671b417e6c68`
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}&lang=KR`
   );
+  let response = await fetch(url);
+  let data = await response.json();
+  weather_main = data.weather[0].main;
+  weather_desc = data.weather[0].description;
+  city = data.name;
+  temp = data.main.temp - 273.15;
+  temp_max = data.main.temp_max - 273.15;
+  temp_min = data.main.temp_min - 273.15;
+  Feels_like = data.main.feels_like - 273.15;
+  console.log(data);
+
+  render();
 };
-console.log(url);
+const render = () => {
+  let weatherHTML = "";
+  weatherHTML = `
+    <p class="city"><span class="material-symbols-outlined">location_on</span> ${city}</p>
+    <p class="weather_icon ${weather_main.toLowerCase()}"></p>
+    <p class="temp">${Math.round(temp * 10) / 10}°C</p>
+    <p class="temp_minmax"><span class="min">${
+      Math.round(temp_min * 10) / 10
+    }°C</span> / <span class="max">${
+    Math.round(temp_max * 10) / 10
+  }°C</span></p>
+    <p class="feels_like">체감온도 ${
+      Math.round(Feels_like * 10) / 10
+    }°C : ${weather_desc}</p>
+    `;
+  document.querySelector(".weather").innerHTML = weatherHTML;
+};
+
+//3.검색기능
+const inputBox = document.querySelector(".input_box");
+const searchOpen = document.querySelector(".search_open");
+const searchInput = document.getElementById("search_input");
+const searchBtn = document.getElementById("search_btn");
+let searchWord;
+searchOpen.addEventListener("click", () => {
+  inputBox.classList.add("on");
+});
+searchBtn.addEventListener("click", () => {
+  searchWord = searchInput.value;
+  searchInput.value = "";
+  console.log(searchWord);
+  getSearchWeather();
+  inputBox.classList.remove("on");
+});
+
+const getSearchWeather = async () => {
+  const API_key = "6dd95cfc5f180ab7bf62671b417e6c68";
+  url = new URL(
+    `https://api.openweathermap.org/data/2.5/weather?q=${searchWord}&appid=${API_key}&lang=KR`
+  );
+  let response = await fetch(url);
+  let data = await response.json();
+  weather_main = data.weather[0].main;
+  weather_desc = data.weather[0].description;
+  city = data.name;
+  temp = data.main.temp - 273.15;
+  temp_max = data.main.temp_max - 273.15;
+  temp_min = data.main.temp_min - 273.15;
+  Feels_like = data.main.feels_like - 273.15;
+  console.log(data);
+
+  render();
+};
